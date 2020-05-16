cf0bc1d04859992388450612a75ad3d86d22efad
azenmoktan <azenmoktan22@gmail.com>
2020-05-16 09:23:49 +0545

    multiauthcompleted

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Supplier;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SupplierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+//        $supplier=Supplier::get();
+//        dd($supplier);
+
+//        $suppplierModel=new Supplier;
+//        $suppplierModel->setConnection('mysql2');
+//        $find=$suppplierModel->find(1);
+//        dd($find);
+//
+        $supplier = DB::connection('mysql2')->table("suppliers")->get();
+        dd($supplier);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Supplier $supplier
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Supplier $supplier)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Supplier $supplier
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Supplier $supplier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Supplier $supplier
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Supplier $supplier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Supplier $supplier
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Supplier $supplier)
+    {
+        //
+    }
+}
